bbd0359cd4ad457f9d73dbd4495e3bf28bba58f4
stanley2058 <stanley2058@yahoo.com.tw>
2025-07-27 20:52:33 +0800

    feat: whoami card & command style

---
+++ src/components/CommandInline.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import { LucideChevronRight } from "lucide-react";
+import { ReactNode } from "react";
+
+export function CommandInline({
+  containerClassName,
+  className,
+  children,
+}: {
+  containerClassName?: string;
+  className?: string;
+  children: ReactNode;
+}) {
+  let content = children;
+  if (typeof children === "string") {
+    const segments = children.split(" ");
+    const binaryName = segments[0];
+    content = (
+      <>
+        <span className="text-ctp-green">{binaryName}</span>{" "}
+        {segments.slice(1).join(" ")}
+      </>
+    );
+  }
+
+  return (
+    <div className={cn("flex flex-row items-center", containerClassName)}>
+      <LucideChevronRight className="text-ctp-yellow" />
+      <span className={className}>{content}</span>
+    </div>
+  );
+}
