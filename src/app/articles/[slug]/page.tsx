37095b6e174152aebe2cbe155720b8184979b36d
stanley2058 <stanley2058@yahoo.com.tw>
2025-11-09 14:56:21 +0800

    feat: metadata for article page

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import fs from "node:fs/promises";
 import path from "node:path";
+import type { Metadata } from "next";
 import { TableOfContent } from "@/components/TableOfContent";
+import { getAllPostWithMeta } from "@/lib/meta";
 
 export default async function Page({
   params,
@@ -22,4 +24,59 @@ export async function generateStaticParams() {
   const files = await fs.readdir(path.join(process.cwd(), "src/content/"));
 
   return files.map((file) => ({ slug: file.replace(".mdx", "") }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+  const posts = await getAllPostWithMeta();
+  const post = posts.find((post) => post.slug === slug);
+  if (!post) {
+    return {
+      title: "Stanley Wang - Blog",
+      description: "My personal blog, tech, travel, and random thoughts.",
+    };
+  }
+
+  const title = `${post.title} - Stanley's Blog`;
+  const meta: Metadata = {
+    title,
+    description: post.description,
+    keywords: post.tags,
+    openGraph: {
+      title,
+      description: post.description,
+      tags: post.tags,
+    },
+    twitter: {
+      title,
+      description: post.description,
+    },
+  };
+
+  if (post.image) {
+    meta.openGraph = {
+      ...meta.openGraph,
+      images: [
+        {
+          url: post.image,
+          alt: post.title,
+        },
+      ],
+    };
+    meta.twitter = {
+      ...meta.twitter,
+      card: "summary_large_image",
+      images: [
+        {
+          url: post.image,
+          alt: post.title,
+        },
+      ],
+    };
+  }
+  return meta;
 }
