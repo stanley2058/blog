28b096631a8b0d8db0a5b439d155230af7cf218a
stanley2058 <stanley2058@yahoo.com.tw>
2025-12-08 21:00:40 +0800

    feat: grid lines & home headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { BlogActions } from "@/components/BlogActions";
 import { Main } from "@/components/Main";
 import { Navbar } from "@/components/Navbar";
 import { TerminalWindow } from "@/components/TerminalWindow";
+import { Text } from "@/components/Text";
 import { TypingCommandInline } from "@/components/TypingCommandInline";
 import { WhoAmICard } from "@/components/WhoAmICard";
 
@@ -15,6 +16,14 @@ export default function Home() {
   return (
     <Main>
       <Navbar className="sticky top-2.5" />
+
+      <div className="my-12 flex flex-col gap-4 text-center">
+        <Text variant="h1">Oh hi!</Text>
+        <Text>
+          I am Stanley, welcome to my blog.
+          <br />I build software and sometimes writes down my journey doing so.
+        </Text>
+      </div>
 
       <TerminalWindow containerClassName="h-100" className="p-2">
         <div className="flex flex-col gap-3">
