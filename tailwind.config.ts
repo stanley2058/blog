18a00446068833a6c8d79ac26a35e84d975a7562
stanley2058 <stanley2058@yahoo.com.tw>
2025-12-09 18:58:59 +0800

    fix: tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
 export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [
     plugin(({ addVariant }) => addVariant("hocus", ["&:hover", "&:focus"])),
   ],
