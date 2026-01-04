88057c0b928a41544f3a6abb0e8cf8a4af410a7d
stanley2058 <stanley2058@yahoo.com.tw>
2026-01-04 14:43:27 +0800

    fix: make the home page as wide as other pages

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,7 +10,7 @@ export function Main({
   return (
     <main
       className={cn(
-        "mx-5 space-y-4 px-5 py-8 pb-4 font-mono",
+        "mx-5 w-full space-y-4 px-5 py-8 pb-4 font-mono",
         "md:mx-auto md:max-w-[808px] md:px-10",
         className,
       )}
